c991d13945c2404cd8c1398551c244c9cb9cacae
Jonathan Xu <jonathanlikestoemail@gmail.com>
2017-03-25 15:14:15 -0400

    made links

--- popup.js
+++ popup.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var button = document.getElementById('action');
-  button.addEventListener('click', function() {
+  var dashboard = document.getElementById('dasboard');
+  var profile = document.getElementById('profile');
+  var preferences = document.getElementById('preferences');
+  dasboard.addEventListener('click', function() {
+    console.log("Test");
+  }, false);
+  profile.addEventListener('click', function() {
+    console.log("Test");
+  }, false);
+  preferences.addEventListener('click', function() {
     console.log("Test");
     chrome.runtime.openOptionsPage();
   }, false);
