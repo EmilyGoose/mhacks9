2f9419b5f9bdae15b53e9279a88e0e88b298c306
Misha Larionov <mishalarionov2@gmail.com>
2017-03-25 00:26:45 -0400

    oh right javascript

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('action');
+  button.addEventListener('click', function() {
+    console.log("Test");
+    });
+  }, false);
+}, false);
