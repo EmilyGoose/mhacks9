02d69236284842914fff807ddbcfa3ca4152c226
Misha Larionov <mishalarionov2@gmail.com>
2017-03-25 16:40:19 -0400

    Added a warning dialog for amazon shopping carts

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -2,11 +2,22 @@
 $(document).ready(function(){
   if ($('input[name=proceedToCheckout]').length > 0) {
     var theBox = $('input[name=proceedToCheckout]');
-    var theFakeBox = $("<div id='theFakeButton'>aaaaaaaa</div>");
+    var theFakeBox = $(
+      "<div id='theFakeBox'>" +
+      "<h1>STOP!</h1>" +
+      "<p>You are about to spend " +
+      "<span class='money'>" +
+      $('.sc-price-sign').html() + "</span>" +
+      "<p>I would tell you how much longer your goal will take " +
+      "but it's not implemented.</p>" +
+      "<p>Are you sure?</p>" +
+      "<button id='userIsSure' " +
+      "onclick='var element = document.getElementById(\"theFakeBox\");" +
+      "element.parentNode.removeChild(element);'>I'm sure!</button>" +
+      "</div>"
+    );
 
     theFakeBox.insertBefore("#gutterCartViewForm");
 
-    //This is how we find the cost but we aren't quite there yet
-    //var cost = alert($('.sc-price-sign').html());
   }
 });
