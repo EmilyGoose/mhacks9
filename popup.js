186bea5a58ec6ac9ca17568fb13b939ba8b878d9
Emily Goose <EmilyGoose@users.noreply.github.com>
2017-03-25 15:08:17 -0400

    Added options

--- popup.js
+++ popup.js
@@ -2,5 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var button = document.getElementById('action');
   button.addEventListener('click', function() {
     console.log("Test");
+    chrome.runtime.openOptionsPage();
   }, false);
 }, false);
