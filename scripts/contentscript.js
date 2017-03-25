517b5e884ada6b20b74e638bf8f4257e8373176e
Emily Goose <EmilyGoose@users.noreply.github.com>
2017-03-25 15:53:01 -0400

    Updated version #

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -6,23 +6,6 @@ $(document).ready(function(){
 
     theFakeBox.insertBefore("#gutterCartViewForm");
 
-    /*
-    $(document).append(theFakeBox);
-
-    var pos = theBox.position();
-
-    theFakeBox.position({
-      my : "left top",
-      at : "left top",
-      of : theBox
-    });
-
-    theFakeBox.height(theBox.height());
-    theFakeBox.width(theBox.width());
-    */
-
-    alert("!");
-
     //This is how we find the cost but we aren't quite there yet
     //var cost = alert($('.sc-price-sign').html());
   }
