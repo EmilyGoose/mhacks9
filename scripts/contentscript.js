5566ec10deefcfe019a32621c2e83c0d17c46c79
Emily Goose <EmilyGoose@users.noreply.github.com>
2017-03-25 14:05:24 -0400

    Started the fake box

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,7 +1,16 @@
 
 $(document).ready(function(){
   if ($('input[name=proceedToCheckout]').length > 0) {
-    var cost = alert($('.sc-price-sign').html());
-    modal.open({content: "This is a checkout page. Stop buying things!"});
+    var theBox = $('input[name=proceedToCheckout]');
+    var theFakeBox = $('<div id="theFakeButton"></div>');
+
+    document.append(theFakeBox);
+
+    
+
+    alert("!");
+
+    //This is how we find the cost but we aren't quite there yet
+    //var cost = alert($('.sc-price-sign').html());
   }
 });
