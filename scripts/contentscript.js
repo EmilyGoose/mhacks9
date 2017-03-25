6d96cdb7d682fe60bbbf750c89a138a1002218a3
Misha Larionov <mishalarionov2@gmail.com>
2017-03-25 15:51:22 -0400

    Really showed Amazon who's boss

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -2,11 +2,24 @@
 $(document).ready(function(){
   if ($('input[name=proceedToCheckout]').length > 0) {
     var theBox = $('input[name=proceedToCheckout]');
-    var theFakeBox = $('<div id="theFakeButton"></div>');
+    var theFakeBox = $("<div id='theFakeButton'>aaaaaaaa</div>");
 
-    document.append(theFakeBox);
+    theFakeBox.insertBefore("#gutterCartViewForm");
 
-    
+    /*
+    $(document).append(theFakeBox);
+
+    var pos = theBox.position();
+
+    theFakeBox.position({
+      my : "left top",
+      at : "left top",
+      of : theBox
+    });
+
+    theFakeBox.height(theBox.height());
+    theFakeBox.width(theBox.width());
+    */
 
     alert("!");
 
