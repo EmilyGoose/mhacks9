d8bf650085bcaef253484c5545e671c5ddf88c7d
Emily Goose <EmilyGoose@users.noreply.github.com>
2017-03-26 09:58:00 -0400

    no more libraries

--- scripts/savegoal.js
+++ scripts/savegoal.js
@@ -13,6 +13,10 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 }, false);
 
+function daysBetween(day1, day2) {
+  return ((day2 - day1) / (1000*60*60*24));
+}
+
 // This is called by #submitForm
 function saveChanges() {
   // Stop the form from submitting
@@ -20,11 +24,29 @@ function saveChanges() {
   // We need to preemptively get incomeHistory and lastIncome to avoid adding duplicates to history
   chrome.storage.sync.get(['incomeHistory', 'lastIncome'], function (items) {
     let history = items.incomeHistory;
-    const lastIncome = items.lastIncome;
+    let lastIncome = items.lastIncome;
+    var calcTotalIncome = items.calculatedTotalIncome;
+
+    if (true) {//lastIncome === undefined) {
+      //Create empty last income for first run
+      lastIncome = {
+        created : Date.now(),
+        period : 'day',
+        value : 0
+      }
+    }
+
+    if (calcTotalIncome === undefined) {
+      calcTotalIncome = 0;
+    }
 
     if (history === undefined) {
       history = [];
     }
+
+   calcTotalIncome += daysBetween(lastIncome.created, Date.now()) *  parseInt(lastIncome.value);
+
+   console.log(daysBetween(lastIncome.created, Date.now()));
 
     // Check to see if you didn't change your income data
     if (lastIncome !== undefined && lastIncome.period == document.getElementById('income-period').value && lastIncome.value == document.getElementById('income').value) {
@@ -43,8 +65,9 @@ function saveChanges() {
         value: document.getElementById('income').value
       });
       // Get a value saved in a form and save it using the Chrome extension storage API.
+
       chrome.storage.sync.set({
-        calculatedTotalIncome: 0, // Misha, write a function to calculate the total or pass in a variable or something
+        calculatedTotalIncome: calcTotalIncome, // Misha, write a function to calculate the total or pass in a variable or something
         lastIncome: {
           created: moment(),
           period: document.getElementById('income-period').value,
@@ -58,6 +81,8 @@ function saveChanges() {
       });
     }
   });
+
+  alert("Saved!");
 }
 
 // Add the click listener to #submitForm in JS
