b66d071288a95550ca2b2c2e97badab53d6dbd0a
Emily Goose <EmilyGoose@users.noreply.github.com>
2017-03-26 10:42:30 -0400

    Saved amount and goal now display on popup

--- popup.js
+++ popup.js
@@ -1,6 +1,34 @@
+function daysBetween(day1, day2) {
+  return Math.round((day2 - day1) / (1000*60*60*24));
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var preferences = document.getElementById('preferences');
   preferences.addEventListener('click', function() {
     chrome.runtime.openOptionsPage();
   }, false);
+
+  chrome.storage.sync.get(['lastIncome', 'calculatedTotalIncome', 'goal'], function (items) {
+    let lastIncome = items.lastIncome;
+    let calcTotalIncome = items.calculatedTotalIncome;
+    let goal = items.goal;
+
+    let saved = calcTotalIncome + daysBetween(lastIncome.created, Date.now()) *  parseInt(lastIncome.value);
+
+    console.log(saved);
+
+    let string = $(
+      "<div id='goalInfo'> Saved: $" +
+      saved  +
+      "</br>" +
+      "Goal: " +
+      goal.price +
+      "</div>"
+    );
+
+    console.log(string);
+
+    string.insertAfter($("#brand"));
+
+  });
 }, false);
